e52ad4cb96847c9ba4db09af2102d7c0a78863fb
gagandayal829ca <gagandayal829ca@gmail.com>
2025-05-29 17:55:34 -0400

    mongoose installed

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(`DB connection successful`);
+  });
 
 const app = require('./app');
 
