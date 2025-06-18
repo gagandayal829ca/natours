131b5faac6ec368d45fff728505b812736260f23
gagandayal829ca <gagandayal829ca@gmail.com>
2025-06-18 10:33:47 -0400

    built in validators implemented

--- models/tourModel.js
+++ models/tourModel.js
@@ -8,6 +8,14 @@ const tourSchema = mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [
+        40,
+        'A tour name must haive less or equal than 40 characters',
+      ],
+      minlength: [
+        40,
+        'A tour name must haive greater or equal than 10 characters',
+      ],
     },
     duration: {
       type: Number,
@@ -23,10 +31,16 @@ const tourSchema = mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
