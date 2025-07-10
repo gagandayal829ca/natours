039e3fe0b6fa97f5043dd463db7ec0abba6aa158
gagandayal829ca <gagandayal829ca@gmail.com>
2025-07-10 11:32:15 -0400

    handling invalid db Id's

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,10 @@
 // Global error handling middleware
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -20,7 +26,7 @@ const sendErrorProd = (err, res) => {
     // Programming or other unknown error: don't leak error details
   } else {
     //  1. Log the error
-    console.error('Error 💣');
+    console.error('Error 💣', err);
 
     // 2. Send the generic message
     res.status(500).json({
@@ -32,18 +38,28 @@ const sendErrorProd = (err, res) => {
 
 module.exports = (err, req, res, next) => {
   //   console.log(err.stack);
-
+  console.log('err', err);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // Creating a hard copy of error object because it is not good practice to
+    // change the argument err directly
+
+    let error = Object.create(err);
+
+    if (error.name === 'CastError') {
+      console.log('inside 2');
+      error = handleCastErrorDB(error);
+    }
+
+    sendErrorProd(error, res);
   }
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  // res.status(err.statusCode).json({
+  //   status: err.status,
+  //   message: err.message,
+  // });
 };
