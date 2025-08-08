b2be23a92586153cb95e0c3f7691c8cbc75dde5a
gagandayal829ca <gagandayal829ca@gmail.com>
2025-08-08 17:53:50 -0400

    security changes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
@@ -9,7 +11,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+// The below will clean any user input from malicious html
+app.use(xss());
 
 // console.log(process.env.NODE_ENV);
 // 1. Global Middlewares
